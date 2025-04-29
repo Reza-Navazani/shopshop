67e8ad24bec1bfcb150be06b6cfabd1f75ceac9b
Reza-navazani <mre.navazani@gmail.com>
2025-04-28 18:28:38 -0700

    port 8000 added

--- dynamic-server/main.py
+++ dynamic-server/main.py
@@ -1,6 +1,7 @@
 # main.py
 # Entry point for the application
 import logging
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from controllers.message_controller import MessageController, message_routes, init_routes
@@ -45,4 +46,5 @@ def handle_exception(e):
     return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
--- dynamic-server/startup.sh
+++ dynamic-server/startup.sh
@@ -1,3 +1,3 @@
 cd /home/site/wwwroot
 python -m pip install -r requirements.txt
-gunicorn --bind=0.0.0.0 --timeout 600 main:app
+gunicorn --bind=0.0.0.0:$PORT --timeout 600 main:app
